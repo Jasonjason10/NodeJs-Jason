6b4650a41acbfdecd2e8ce7c41548a232cbaefb9
Jasonjason10 <ofil0001@hz.nl>
2023-01-19 13:02:20 +0100

    fixed error

--- app.js
+++ app.js
@@ -8,35 +8,9 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: "public"})
 })
-app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: "public"})
-})
 
-app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: "public"})
-})
-
-app.use(express.static("public"));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: "public"})
-})
-
-app.use(express.static("public"));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: "public"})
-})
-
-app.use(express.static("public"));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: "public"})
-})
 
 
 app.listen(port, () => {
